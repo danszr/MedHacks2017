600ec52346dd35ca70d3cc0b02b12651c11eac2b
Dan Selzer <dselzer@optonline.net>
2017-09-09 15:16:30 -0400

    changed starter pack

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
+import { MessageService } from "./messages/message.service";
+
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-
 }
