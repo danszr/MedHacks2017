b33d141b47a28ae8aff2ee5199b5fe98e810dc21
Dan Selzer <dselzer@optonline.net>
2017-12-02 19:31:53 -0500

    removed vulnerability

--- assets/app/auth/twilio.service.ts
+++ assets/app/auth/twilio.service.ts
@@ -1,7 +1,7 @@
 // var twilio = require('twilio');
 //
-// var accountSid = 'AC6a91356ab1e1e61819c584c385a363a9'; // Your Account SID from www.twilio.com/console
-// var authToken = 'f879d64dcb4335ce78d9effa0a9596b9';   // Your Auth Token from www.twilio.com/console
+// var accountSid = 'blah'; // Your Account SID from www.twilio.com/console
+// var authToken = 'blah';   // Your Auth Token from www.twilio.com/console
 //
 // var client = twilio(accountSid, authToken);
 //
